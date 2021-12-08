d04fd9812b0fdcb470b52f6066adb16d5aeffb03
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-12-08 13:56:57 -0800

    Set up API hook call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(resp => {
+        console.log(resp.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA Photo of the Day Generator</h1>
+      <h3>Photo Title</h3>
     </div>
   );
 }
